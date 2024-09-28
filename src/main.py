140d7de53db5466828a8c955449efe9b4d4696c0
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-09-28 15:34:56 -0300

    feat: add textnode

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from textnode import TextNode
+
+print(TextNode("This is a text node", "bold", "https://www.boot.dev"))
---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+class TextNode():
+    def __init__(self, text, text_type, url = None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
