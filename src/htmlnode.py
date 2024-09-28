85d114ed657197f83647dc98b28bcf38d1e6dff2
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-09-28 20:29:13 -0300

    chore: use is for pythonic code

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,7 +9,7 @@ class HTMLNode():
         raise NotImplementedError
 
     def props_to_html(self):
-        if self.props == None or len(self.props) == 0:
+        if self.props is None or len(self.props) == 0:
             return ""
 
         return "".join([f" {item}='{self.props[item]}'" for item in self.props])
--- src/leafnode.py
+++ src/leafnode.py
@@ -6,9 +6,9 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.value == None:
+        if self.value is None:
             raise ValueError('Invalid HTML: missing value')
-        if self.tag == None:
+        if self.tag is None:
             return self.value
 
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
